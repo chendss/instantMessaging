bc12d94281872e4508249e81cfd80aaf7cc95f5f
dashao <capslock.ch@outlook.com>
2019-06-22 20:14:12 +0800

    初始化

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var a = 'ces';
+    var a = '你好世界';
     return new MaterialApp(
       title: '导航标题',
       home: new Scaffold(
