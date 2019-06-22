31a6b19f9f785a91d78c58230fdcbb88702eb6a4
dashao <capslock.ch@outlook.com>
2019-06-22 20:13:57 +0800

    初始化

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var a = 'ces';
+    return new MaterialApp(
+      title: '导航标题',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('导航标题'),
+        ),
+        body: new Center(
+          child: new Text(a),
+        ),
+      ),
+    );
+  }
+}
